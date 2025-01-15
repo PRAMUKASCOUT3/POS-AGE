a47891f2f65d3e55dc6799a16edc7dd2c5cdfbbf
AndiSAG01 <andisabaniabdulgani22@gmail.com>
2025-01-15 22:26:18 +0700

    Update Locked

--- resources/views/partials/time.blade.php
+++ resources/views/partials/time.blade.php
@@ -16,7 +16,7 @@
     }
 </style>
 
-@if (now() >= Carbon\carbon::parse('2025-3-30'))
+@if (now() >= Carbon\carbon::parse('2025-2-15'))
     <div id="overlay" class="overlay">
         <p>Maaf, tombol dan layar tidak dapat diakses saat ini.
         <p>
